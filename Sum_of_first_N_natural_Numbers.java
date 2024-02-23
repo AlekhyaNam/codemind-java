8f718812ad16510b2c34ca8ab8ef76f8cd21cfca
AlekhyaNam <119840397+AlekhyaNam@users.noreply.github.com>
2024-02-23 11:47:14 +0530

    import java.util.*; public class Solution{     public static void main(String[] args){         Scanner sc= new Scanner(System.in);         int n=sc.nextInt();         int s=n*(n+1)/2;         System.out.print(s);     } }

---
+++ Sum_of_first_N_natural_Numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=n*(n+1)/2;
+        System.out.print(s);
+    }
+}
