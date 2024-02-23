2838eaded998f0c1c681e6460c7e7a09232e827b
AlekhyaNam <119840397+AlekhyaNam@users.noreply.github.com>
2024-02-23 11:57:47 +0530

    import java.util.*; public class Solution{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int m = sc.nextInt();         for(int i=n;i<=m;i++){             System.out.print(i+" ");         }     } }

---
+++ A_to_B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for(int i=n;i<=m;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
